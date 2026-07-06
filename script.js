90d4c09e02acce1bdf21c661ad973bfc21ae2781
“xymurgist” <pps.jim@gmail.com>
2026-07-06 14:09:38 -0600

    Create function to return computer's choice
    Using Math.random and an if statement,
    return one of the following strings:  "rock", "paper", "scissors"

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ console.log("I'm rich!");
 // console.log(currentYear); // Outputs the 4-digit year (e.g., 2026)
 
 document.getElementById("copyright").innerText = new Date().getFullYear();
+
+
+// randomly return one of the following strings: "rock", "paper", "scissors"
+// consider Math.random()
+// do not use an array
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+
+    if (randomNum == 0) {
+        return computerChoice = "rock";
+    } else if (randomNum == 1) {
+        return computerChoice = "paper";
+    } else if (randomNum == 2) {
+        return computerChoice = "scissors";
+    } else {
+        return computerChoice = "Something went horribly wrong!"
+    }
+}
+console.log(getComputerChoice());
+
