634a21571a4aac8b1fc1204b2dee0f768b647089
“xymurgist” <pps.jim@gmail.com>
2026-07-06 13:52:23 -0600

    Initialize index.html and script.js files
    Created index.html and script.js files, and they are ready for code.

---
+++ script.js
@@ -0,0 +1,11 @@
+// write pseudocode
+// write code
+// test code
+
+// create console output to confirm script is seen by html
+console.log("I'm rich!");
+
+// const currentYear = new Date().getFullYear();
+// console.log(currentYear); // Outputs the 4-digit year (e.g., 2026)
+
+document.getElementById("copyright").innerText = new Date().getFullYear();
